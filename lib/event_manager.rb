375b6bbc95e80bcdc16e7de080b373245daaed09
Fernando Paredes <nanoid.xd@gmail.com>
2013-05-09 13:17:32 -0700

    Add welcome letter.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -15,6 +15,7 @@ def legislators_by_zipcode(zipcode)
     "#{legislator.first_name} #{legislator.last_name} "
   end
 end
+
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
   name = row[:first_name]
