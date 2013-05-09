5a723fa98ed885576a3142d87fb21ec518c259e8
Fernando Paredes <nanoid.xd@gmail.com>
2013-05-09 13:09:40 -0700

    Refactor legislator lookup

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,18 +8,19 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
 
+def legislators_by_zipcode(zipcode)
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+  legislator_names = legislators.collect do |legislator|
+    "#{legislator.first_name} #{legislator.last_name} "
+  end
+end
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
 
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name} "
-  end
-
-  legislators_string = legislator_names.join(", ")
-  puts "#{name} #{zipcode} #{legislators_string}"
+  legislators = legislators_by_zipcode(zipcode).join(", ")
+  puts "#{name} #{zipcode} #{legislators}"
 end
